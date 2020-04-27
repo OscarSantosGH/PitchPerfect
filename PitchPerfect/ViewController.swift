ccf2b70d14db83b267947a1886481a07e46d26b4
Oscar Santos <oscarsantos.go@gmail.com>
2020-04-27 19:43:56 -0400

    add: labels and buttons in storyboard

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func recordButtonTap(_ sender: Any) {
+        recordLabel.text = "Recording..."
+    }
+    
+    @IBAction func stopButtonTap(_ sender: Any) {
+        
+    }
+    
 }
 
